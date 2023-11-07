7fee4ccfd76b70146a93cf94c73abf8de2488ad8
Antonio Nardella <antonio.nardella@iota.org>
2023-11-08 00:31:52 +0100

    little optimization

--- helpers/smr_market_data.py
+++ helpers/smr_market_data.py
@@ -319,53 +319,43 @@ async def get_shimmer_data():
 
 async def build_embed():
     """Here we save a pickel file for the Discord embed message"""
-    # Execute functions to get data
-    current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
-    coingecko_data = await get_coingecko_exchange_data()
-    defillama_data = await get_defillama_data()
-    geckoterminal_data = await get_geckoterminal_data()
-    shimmer_data = await get_shimmer_data()
-    bitfinex_order_book_data = await get_bitfinex_book_data(coingecko_data["usd_price"])
+    try:
+        # Get data from API calls
+        current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
+        coingecko_data = await get_coingecko_exchange_data()
+        defillama_data = await get_defillama_data()
+        geckoterminal_data = await get_geckoterminal_data()
+        shimmer_data = await get_shimmer_data()
+        bitfinex_order_book_data = await get_bitfinex_book_data(coingecko_data['usd_price'])
+        total_defi_tx_24h = geckoterminal_data["total_defi_tx_24h"]
+        shimmer_rank = defillama_data["shimmer_rank"]
 
-    # Format variables that need currency formatting
-    formatted_usd_price = await format_currency(coingecko_data["usd_price"])
-    formatted_volume = await format_currency(coingecko_data["total_volume"])
-    formatted_defi_volume = await format_currency(geckoterminal_data["defi_total_volume"])
-    formatted_tvl = await format_currency(defillama_data["shimmer_tvl"])
-    # Format glow to SMR
-    shimmer_onchain_token_amount = await format_shimmer_amount(shimmer_data["shimmer_onchain_token_amount"])
-    # Format the integer as a value with a currency separator
-    formatted_shimmer_onchain_token_amount = await format_currency(shimmer_onchain_token_amount, "SMR")
- 
-    # Set up Bitfinex order book depth
-    # Initialize empty strings for different percentage levels
-    positive_order_book_depth_str_2_percent = ""
-    negative_order_book_depth_str_2_percent = ""
-    positive_order_book_depth_str_5_percent = ""
-    negative_order_book_depth_str_5_percent = ""
-    positive_order_book_depth_str_10_percent = ""
-    negative_order_book_depth_str_10_percent = ""
-    positive_order_book_depth_str_20_percent = ""
-    negative_order_book_depth_str_20_percent = ""
+        # Set up Bitfinex order book depth
+        positive_order_book_depth_str_2_percent = ""
+        negative_order_book_depth_str_2_percent = ""
+        positive_order_book_depth_str_5_percent = ""
+        negative_order_book_depth_str_5_percent = ""
+        positive_order_book_depth_str_10_percent = ""
+        negative_order_book_depth_str_10_percent = ""
+        positive_order_book_depth_str_20_percent = ""
+        negative_order_book_depth_str_20_percent = ""
 
-    # Iterate through the order book data and format the strings
-    for percentage, data in bitfinex_order_book_data['order_book_depth'].items():
-        # Format the 'buy' data using format_currency() function
-        if 'buy' in data:
-            formatted_buy_data = await format_currency(data['buy'], "SMR")
-            buy_data = f"Buy: {formatted_buy_data}\n\n"
-        else:
-            logger.error(f"Missing 'buy' key for percentage level {percentage}")
-        # Format the 'sell' data using format_currency() function
-        if 'sell' in data:
-            formatted_sell_data = await format_currency(data['sell'], "SMR")
-            sell_data = f"Sell: {formatted_sell_data}\n\n"
-        else:
-            logger.error(f"Missing 'sell' key for percentage level {percentage}")
+        # Iterate through the order book data and format the strings
+        for percentage, data in bitfinex_order_book_data['order_book_depth'].items():
+            # Format the 'buy' data using format_currency() function
+            if 'buy' in data:
+                formatted_buy_data = await format_currency(data['buy'], "SMR")
+                buy_data = f"Buy: {formatted_buy_data}\n\n"
+            else:
+                logger.error(f"Missing 'buy' key for percentage level {percentage}")
+            # Format the 'sell' data using format_currency() function
+            if 'sell' in data:
+                formatted_sell_data = await format_currency(data['sell'], "SMR")
+                sell_data = f"Sell: {formatted_sell_data}\n\n"
+            else:
+                logger.error(f"Missing 'sell' key for percentage level {percentage}")
 
-        # Check if the percentage is negative and append buy data only
-        if percentage.startswith('-'):
-            buy_sell_info = f"**{percentage}**:\n{buy_data}"
+            buy_sell_info = f"**{percentage}**:\n{buy_data if percentage.startswith('-') else sell_data}"
             if int(percentage[:-1]) == -2:
                 negative_order_book_depth_str_2_percent += buy_sell_info
             elif int(percentage[:-1]) == -5:
@@ -374,10 +364,7 @@ async def build_embed():
                 negative_order_book_depth_str_10_percent += buy_sell_info
             elif int(percentage[:-1]) == -20:
                 negative_order_book_depth_str_20_percent += buy_sell_info
-        # Check if the percentage is positive and append sell data only
-        else:
-            buy_sell_info = f"**{percentage}**:\n{sell_data}"
-            if int(percentage[:-1]) == 2:
+            elif int(percentage[:-1]) == 2:
                 positive_order_book_depth_str_2_percent += buy_sell_info
             elif int(percentage[:-1]) == 5:
                 positive_order_book_depth_str_5_percent += buy_sell_info
@@ -386,59 +373,36 @@ async def build_embed():
             elif int(percentage[:-1]) == 20:
                 positive_order_book_depth_str_20_percent += buy_sell_info
 
-    # Setup variables for the embed
-    try:
-        usd_price = formatted_usd_price
-        total_volume = formatted_volume
-        defi_total_volume = formatted_defi_volume
-        total_defi_tx_24h = geckoterminal_data["total_defi_tx_24h"]
-        shimmer_tvl = formatted_tvl
-        shimmer_rank = defillama_data["shimmer_rank"]
-        shimmer_onchain_token_amount = formatted_shimmer_onchain_token_amount
-        bitfinex_order_book_data = bitfinex_order_book_data["order_book_depth"]
-
         # Create an embed instance
         embed = discord.Embed(title="Shimmer Market Data", color=0x00FF00)
-
-        # Add fields to the embed
-        embed.add_field(name="Price (Coingecko)", value=usd_price, inline=False)
-        embed.add_field(name="24h Volume (Bitfinex)", value=total_volume, inline=False)
-        # Add a blank field for separation
+        embed.add_field(name="Price (Coingecko)", value=f"{await format_currency(coingecko_data['usd_price'])}", inline=False)
+        embed.add_field(name="24h Volume (Bitfinex)", value=f"{await format_currency(coingecko_data['total_volume'])}", inline=False)
         embed.add_field(name="\u200b", value="\u200b", inline=False)
         embed.add_field(name="Defi Data", value="\u200b", inline=False)
         embed.add_field(name="Shimmer Rank (DefiLlama)", value=shimmer_rank, inline=True)
-        embed.add_field(name="Shimmer Onchain Amount (Shimmer API)", value=shimmer_onchain_token_amount, inline=True)
-        embed.add_field(name="Total Value Locked (DefiLlama)", value=shimmer_tvl, inline=True)
+        embed.add_field(name="Shimmer Onchain Amount (Shimmer API)", value=f"{await format_currency(await format_shimmer_amount(shimmer_data['shimmer_onchain_token_amount']), 'SMR')}", inline=True)
+        embed.add_field(name="Total Value Locked (DefiLlama)", value=f"{await format_currency(defillama_data['shimmer_tvl'])}", inline=True)
         embed.add_field(name="24h DeFi Transactions (DefiLlama)", value=total_defi_tx_24h, inline=True)
-        embed.add_field(name="24h DeFi Volume (GeckoTerminal)", value=defi_total_volume, inline=True)
-        # Add a blank field for separation
+        embed.add_field(name="24h DeFi Volume (GeckoTerminal)", value=f"{await format_currency(geckoterminal_data['defi_total_volume'])}", inline=True)
         embed.add_field(name="\u200b", value="\u200b", inline=False)
         embed.add_field(name="ShimmerEVM Order Books", value="\u200b", inline=False)
         embed.add_field(name="Order Book depth ±2%", value=f"{negative_order_book_depth_str_2_percent} {positive_order_book_depth_str_2_percent}", inline=True)
-        # embed.add_field(name="\u200b", value=positive_order_book_depth_str_2_percent, inline=True)
-        
         embed.add_field(name="\u200b", value="\u200b", inline=False)
         embed.add_field(name="Order Book depth ±5%", value=f"{negative_order_book_depth_str_5_percent} {positive_order_book_depth_str_5_percent}", inline=True)
-        # embed.add_field(name="\u200b", value=positive_order_book_depth_str_5_percent, inline=True)
-        
         embed.add_field(name="\u200b", value="\u200b", inline=False)
         embed.add_field(name="Order Book depth ±10%", value=f"{negative_order_book_depth_str_10_percent} {positive_order_book_depth_str_10_percent}", inline=True)
-        # embed.add_field(name="\u200b", value=positive_order_book_depth_str_10_percent, inline=True)
-        
         embed.add_field(name="\u200b", value="\u200b", inline=False)
         embed.add_field(name="Order Book depth ±20%", value=f"{negative_order_book_depth_str_20_percent} {positive_order_book_depth_str_20_percent}", inline=True)
-        # embed.add_field(name="\u200b", value=positive_order_book_depth_str_20_percent, inline=True)
-        # Add a blank field for separation
         embed.add_field(name="\u200b", value="\u200b", inline=False)
 
         # Add additional information
         embed.add_field(name="Sources", value="Bitfinex, Coingecko, DefiLlama, GeckoTerminal, Shimmer API", inline=False)
-
-        # Set the footer
         embed.set_footer(text="Data updated every 24h; last updated: " + current_time + "\nMade with IOTA-❤️ by Antonio\nOut of beta SOON™")
 
+        # Save the embed to a pickle file
         with open("assets/embed_shimmer_market_data.pkl", "wb") as f:
             pickle.dump(embed, f)
+
     except Exception:
         logger.info(traceback.format_exc())
 
