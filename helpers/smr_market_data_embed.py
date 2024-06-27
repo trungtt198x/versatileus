4eee0aa2086d40c1d81bd13fe75124503c6ef699
trung <trungtt1981@gmail.com>
2024-06-27 13:52:49 +0200

    Post data to Slack channel

--- helpers/smr_market_data_embed.py
+++ helpers/smr_market_data_embed.py
@@ -11,6 +11,8 @@ import datetime
 import pickle
 import traceback
 import requests
+import json
+import datetime
 import helpers.configuration_manager as configuration_manager
 from helpers.formatting import format_currency, format_shimmer_amount, generate_discord_timestamp
 from helpers.smr_market_data.smd_bitfinex import calculate_total_bitfinex_depth
@@ -100,27 +102,45 @@ async def build_embed():
 
         # Create an embed instance
         embed = discord.Embed(title="IOTA Market Data", color=0x00FF00)
-        slack_data = "IOTA Market Data\n"
+        slack_data = [{"type": "header", "text": {"type": "plain_text", "text": "IOTA Market Data"}}]
 
-        coin_gecko_usd_price = await format_currency(coingecko_data['usd_price'])
-        embed.add_field(name="Price (Coingecko)", value=f"{coin_gecko_usd_price}", inline=False)
-        slack_data += "Price (Coingecko)\n"
-        slack_data += coin_gecko_usd_price
+        my_coin_gecko_usd_price = await format_currency(coingecko_data['usd_price'])
+        embed.add_field(name="Price (Coingecko)", value=f"{my_coin_gecko_usd_price}", inline=False)
+        slack_data.append({"type": "section", "text": {"type": "mrkdwn", "text": "*Price (Coingecko)*\n" + my_coin_gecko_usd_price }})
 
         # embed.add_field(name="24h Volume (Bitfinex)", value=f"{await format_currency(coingecko_data['total_volume'])}", inline=False)
-        embed.add_field(name="24h Volume (Coingecko)", value=f"{await format_currency(coingecko_24h_vol)}", inline=False)
+        
+        my_coingecko_24h_vol = await format_currency(coingecko_24h_vol)
+        embed.add_field(name="24h Volume (Coingecko)", value=f"{my_coingecko_24h_vol}", inline=False)
+        slack_data.append({"type": "section", "text": {"type": "mrkdwn", "text": "*24h Volume (Coingecko)*\n" + my_coingecko_24h_vol }})
+
         embed.add_field(name="\u200b", value="\u200b", inline=False)
-        embed.add_field(name="Defi Data", value="\u200b", inline=False)
+        # slack_data.append({"type": "section", "text": {"type": "mrkdwn", "text": "\n" }})
+        slack_data.append({"type": "divider"})
+
+        embed.add_field(name="DeFi Data", value="\u200b", inline=False)
+        slack_data.append({"type": "section", "text": {"type": "mrkdwn", "text": "*DeFi Data*\n" }})
+
         embed.add_field(name="IOTA Rank (DefiLlama)", value=iota_rank, inline=True)
+        slack_data.append({"type": "section", "text": {"type": "mrkdwn", "text": "*IOTA Rank (DefiLlama)*\n" + str(iota_rank) }})
+
         try:
-            embed.add_field(name="IOTA on-chain amount (IOTA API)", value=f"{await format_currency(await format_shimmer_amount(shimmer_data['shimmer_onchain_token_amount']), 'IOTA')}", inline=True)
+            my_shimmer_onchain_token_amount = await format_currency(await format_shimmer_amount(shimmer_data['shimmer_onchain_token_amount']), 'IOTA')
+            embed.add_field(name="IOTA on-chain amount (IOTA API)", value=f"{my_shimmer_onchain_token_amount}", inline=True)
+            slack_data.append({"type": "section", "text": {"type": "mrkdwn", "text": "*IOTA on-chain amount (IOTA API)*\n" + my_shimmer_onchain_token_amount }})
         except Exception:
             logger.info(traceback.format_exc())
 
-        embed.add_field(name="Total Value Locked (DefiLlama)", value=f"{await format_currency(defillama_data['iota_tvl'])}", inline=True)
+        my_iota_tvl = await format_currency(defillama_data['iota_tvl'])
+        embed.add_field(name="Total Value Locked (DefiLlama)", value=f"{my_iota_tvl}", inline=True)
+        slack_data.append({"type": "section", "text": {"type": "mrkdwn", "text": "*Total Value Locked (DefiLlama)*\n" + str(my_iota_tvl) }})
+
         # embed.add_field(name="24h DeFi Transactions (GeckoTerminal)", value=total_defi_tx_24h, inline=True)
         # embed.add_field(name="24h DeFi Volume (GeckoTerminal)", value=f"{await format_currency(geckoterminal_data['defi_total_volume'])}", inline=True)
         embed.add_field(name="\u200b", value="\u200b", inline=False)
+        # slack_data.append({"type": "section", "text": {"type": "mrkdwn", "text": "\n" }})
+        slack_data.append({"type": "divider"})
+
         # embed.add_field(name="ShimmerEVM Order Books", value="\u200b", inline=False)
         # embed.add_field(name="Order Book depth ±2%", value=f"{negative_order_book_depth_str_2_percent} {positive_order_book_depth_str_2_percent}", inline=True)
         # embed.add_field(name="\u200b", value="\u200b", inline=False)
@@ -132,9 +152,15 @@ async def build_embed():
         # embed.add_field(name="\u200b", value="\u200b", inline=False)
 
         # Add additional information
-        embed.add_field(name="Sources", value="Bitfinex, Coingecko, DefiLlama, GeckoTerminal, IOTA API", inline=False)
+        my_source = "Bitfinex, Coingecko, DefiLlama, GeckoTerminal, IOTA API"
+        embed.add_field(name="Sources", value=f"{my_source}", inline=False)
+        slack_data.append({"type": "section", "text": {"type": "mrkdwn", "text": "*Sources*\n" + my_source }})
+
         embed.add_field(name="Last Data Update", value=f"{discord_timestamp}", inline=False)
+        slack_data.append({"type": "section", "text": {"type": "mrkdwn", "text": "*Last Data Update*\n" + datetime.datetime.now().strftime("%d/%m/%Y %H:%M")}})
+
         embed.set_footer(text="Data updated every 24h\nMade with IOTA-❤️ by Holger and Mido")
+        slack_data.append({"type": "section", "text": {"type": "plain_text", "text": "Data updated every 24h\n" }})
 
         # Save the embed to a pickle file
         with open("assets/embed_shimmer_market_data.pkl", "wb") as f:
@@ -142,7 +168,31 @@ async def build_embed():
 
         # Write to slack channel
         # Must have double quotes around slack_data
-        slack_data = '{"text": ' + '"' + slack_data + '"' + '}'
+        # slack_data = '{"text": ' + '"' + slack_data + '"' + '}'
+
+        # slack_data = [
+    	# {
+    	# 	"type": "section",
+    	# 	"text": {
+    	# 		"type": "mrkdwn",
+    	# 		"text": "Danny Torrence left the following review for your property:"
+    	# 	}
+    	# },
+    	# {
+    	# 	"type": "section",
+    	# 	"block_id": "section567",
+    	# 	"text": {
+    	# 		"type": "mrkdwn",
+    	# 		"text": "<https://example.com|Overlook Hotel> \n :star: \n Doors had too many axe holes, guest in room 237 was far too rowdy, whole place felt stuck in the 1920s."
+    	# 	},
+    	# 	"accessory": {
+    	# 		"type": "image",
+    	# 		"image_url": "https://is5-ssl.mzstatic.com/image/thumb/Purple3/v4/d3/72/5c/d3725c8f-c642-5d69-1904-aa36e4297885/source/256x256bb.jpg",
+    	# 		"alt_text": "Haunted hotel image"
+    	# 	}
+    	# }]
+
+        slack_data = '{"blocks": ' + json.dumps(slack_data) + '}'
         logger.info(slack_data)
         res = requests.post(url=slack_channel, data=slack_data)
         logger.info(res)
