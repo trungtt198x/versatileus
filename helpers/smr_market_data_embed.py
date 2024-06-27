018bebbbe48b7d223710a09ff330cefc1bcfa961
trung <trungtt1981@gmail.com>
2024-06-27 21:50:18 +0200

    Cleanup

--- helpers/smr_market_data_embed.py
+++ helpers/smr_market_data_embed.py
@@ -173,36 +173,11 @@ async def build_embed():
         with open("assets/embed_shimmer_market_data.pkl", "wb") as f:
             pickle.dump(embed, f)
 
-        # Write to slack channel
-        # Must have double quotes around slack_data
-        # slack_data = '{"text": ' + '"' + slack_data + '"' + '}'
-
-        # slack_data = [
-    	# {
-    	# 	"type": "section",
-    	# 	"text": {
-    	# 		"type": "mrkdwn",
-    	# 		"text": "Danny Torrence left the following review for your property:"
-    	# 	}
-    	# },
-    	# {
-    	# 	"type": "section",
-    	# 	"block_id": "section567",
-    	# 	"text": {
-    	# 		"type": "mrkdwn",
-    	# 		"text": "<https://example.com|Overlook Hotel> \n :star: \n Doors had too many axe holes, guest in room 237 was far too rowdy, whole place felt stuck in the 1920s."
-    	# 	},
-    	# 	"accessory": {
-    	# 		"type": "image",
-    	# 		"image_url": "https://is5-ssl.mzstatic.com/image/thumb/Purple3/v4/d3/72/5c/d3725c8f-c642-5d69-1904-aa36e4297885/source/256x256bb.jpg",
-    	# 		"alt_text": "Haunted hotel image"
-    	# 	}
-    	# }]
-
+        # Post data to slack channel
         slack_data = '{"blocks": ' + json.dumps(slack_data) + '}'
-        logger.info(slack_data)
+        # logger.info(slack_data)
         res = requests.post(url=slack_channel, data=slack_data)
-        logger.info(res)
+        # logger.info(res)
     except Exception:
         logger.info(traceback.format_exc())
 
